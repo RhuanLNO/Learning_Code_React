2d22249728dd70684b68d59485c96e0315f69fe6
Rhuan Levy <rhuan_os@hotmail.com>
2022-07-21 00:43:34 -0300

    Changed the login function, added functions to format date and time received from the back-end

--- form/src/Utils.ts
+++ form/src/Utils.ts
@@ -7,29 +7,52 @@ const location = () => {
 }
 
 const login = () => {
-    axios.post('https://generic-api-nodejs-sequelize.herokuapp.com/users/login', {
+    return axios.post('https://generic-api-nodejs-sequelize.herokuapp.com/users/login', {
       email: 'rhuan_os@hotmail.com',
       password: '12345678'
     })
-    .then((response) => { 
-      const TOKEN = response.data.token;
-      axios.get('https://generic-api-nodejs-sequelize.herokuapp.com/users', { headers: { authorization: TOKEN } })
+/*     .then((response) => {
+      return response.data.token;
+/*       axios.get('https://generic-api-nodejs-sequelize.herokuapp.com/users', { headers: { authorization: TOKEN } })
         .then((response) => {
             let user = response.data.filter((users: any) => {
-              return users.id === 38;
+              return users;
             });
-            console.log(user);
-            axios.delete(`https://generic-api-nodejs-sequelize.herokuapp.com/users/${user[0].id}`, {
-              headers: {
-                authorization: TOKEN
-              }
-            })
-            .then(() => console.log("Usuário deletado"))
-            .catch(() => console.log("Não foi possível deletar o usuário"));  
-          })
-        .catch(() => { console.log("Não foi possível carregar a lista de usuários") })
-      })
-    .catch(() => { console.log("Não foi possível realizar o login") });
+            console.log(user); */
+//            axios.delete(`https://generic-api-nodejs-sequelize.herokuapp.com/users/${user[0].id}`, {
+//              headers: {
+//                authorization: TOKEN
+//              }
+//            })
+//            .then(() => console.log("Usuário deletado"))
+//            .catch(() => console.log("Não foi possível deletar o usuário"));  
+/*           })
+        .catch(() => { console.log("Não foi possível carregar a lista de usuários") }) */
+/*       })
+    .catch(() => { 
+      console.log("Não foi possível realizar o login"); 
+    });
+    console.log(TOKEN);
+    return TOKEN; */
 }
 
-export { location, login };
+const formatDate = (date: string) => {
+  let calendar = {
+    time: '',
+    date: '',
+  };
+  const splittedValues = date.split("T");
+  calendar.time = formatTime(splittedValues[1] as any);
+  const splittedDate = splittedValues[0].split("-");
+  calendar.date = `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  return calendar;
+}
+
+const formatTime = (time:string) => {
+  const splittedTime = time.split(":");
+  return `${splittedTime[0]}:${splittedTime[1]}`;
+};
+
+export { location, login, formatDate };
+
+//linha 19 .id === 38
